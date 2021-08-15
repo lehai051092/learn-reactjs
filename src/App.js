aefef0644b92b391d122b3704dd935b75b49effd
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-15 17:29:20 +0700

    demo get api products

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ import Counter from './components/Counter';
 import TodoFeature from './features/Todo/pages';
 import { NavLink, Route, Switch } from 'react-router-dom';
 import NotFound from './components/NotFound';
+import { useEffect } from 'react';
+import productApi from './api/productApi';
 
 function App() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10,
+      };
+      const productList = await productApi.getAll(params);
+      console.log(productList);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
       Header
