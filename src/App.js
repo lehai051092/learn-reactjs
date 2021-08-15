2e88f336a196dfd093d9d02fc54918aa5d0c646f
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-15 15:33:02 +0700

    config Prettier

--- src/App.js
+++ src/App.js
@@ -3,30 +3,39 @@ import AlbumFeature from './features/Album/pages';
 import ColorBox from './components/ColorBox';
 import Counter from './components/Counter';
 import TodoFeature from './features/Todo/pages';
-import {NavLink, Route, Switch} from "react-router-dom";
-import NotFound from "./components/NotFound";
+import { NavLink, Route, Switch } from 'react-router-dom';
+import NotFound from './components/NotFound';
 
 function App() {
-    return (
-        <div className="App">
-            Header
-            {/*nav link*/}
-            <p><NavLink to="/todos" activeClassName='active-menu'>Todos</NavLink></p>
-            <p><NavLink to="/albums">Albums</NavLink></p>
-            <p><NavLink to="/color-box">Color Box</NavLink></p>
-            <p><NavLink to="/counter">Counter</NavLink></p>
-
-            <Switch>
-                <Route path="/todos" component={TodoFeature}/>
-                <Route path="/albums" component={AlbumFeature}/>
-                <Route path="/color-box" component={ColorBox}/>
-                <Route path="/counter" component={Counter}/>
-                {/*Not Found*/}
-                <Route component={NotFound}/>
-            </Switch>
-            Footer
-        </div>
-    );
+  return (
+    <div className="App">
+      Header
+      {/*nav link*/}
+      <p>
+        <NavLink to="/todos" activeClassName="active-menu">
+          Todos
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to="/albums">Albums</NavLink>
+      </p>
+      <p>
+        <NavLink to="/color-box">Color Box</NavLink>
+      </p>
+      <p>
+        <NavLink to="/counter">Counter</NavLink>
+      </p>
+      <Switch>
+        <Route path="/todos" component={TodoFeature} />
+        <Route path="/albums" component={AlbumFeature} />
+        <Route path="/color-box" component={ColorBox} />
+        <Route path="/counter" component={Counter} />
+        {/*Not Found*/}
+        <Route component={NotFound} />
+      </Switch>
+      Footer
+    </div>
+  );
 }
 
 export default App;
--- src/features/Todo/pages/index.jsx
+++ src/features/Todo/pages/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import {Route, Switch, useRouteMatch} from "react-router-dom";
-import ListPage from "./ListPage";
-import DetailPage from "./DetailPage";
-import NotFound from "../../../components/NotFound";
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import ListPage from './ListPage';
+import DetailPage from './DetailPage';
+import NotFound from '../../../components/NotFound';
 
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-    const match = useRouteMatch(); // nested routing
-    return (
-        <div>
-            <Switch>
-                <Route path={match.path} component={ListPage} exact/>
-                <Route path={`${match.path}/:todoId`} component={DetailPage} exact/>
-                <Route component={NotFound}/>
-            </Switch>
-        </div>
-    );
+  const match = useRouteMatch(); // nested routing
+  return (
+    <div>
+      <Switch>
+        <Route path={match.path} component={ListPage} exact />
+        <Route path={`${match.path}/:todoId`} component={DetailPage} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  );
 }
 
 export default TodoFeature;
