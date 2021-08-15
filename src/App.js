807a6a8f93e096ffe6d13571028868d4bf4bc2da
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-15 14:18:33 +0700

    Create routing example

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import AlbumFeature from './features/Album/pages';
 import ColorBox from './components/ColorBox';
 import Counter from './components/Counter';
 import TodoFeature from './features/Todo/pages';
+import {Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <TodoFeature />
-      <ColorBox />
-      <Counter />
-      <AlbumFeature />
-    </div>
-  );
+    return (
+        <div className="App">
+            Header
+            <Route path="/todos" component={TodoFeature}/>
+            <Route path="/albums" component={AlbumFeature}/>
+            <Route path="/color-box" component={ColorBox}/>
+            <Route path="/counter" component={Counter}/>
+            Footer
+        </div>
+    );
 }
 
 export default App;
