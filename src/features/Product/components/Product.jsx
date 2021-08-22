1025bccf44ae8b813efac8799d0802bb4edd34d1
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-22 20:26:46 +0700

    Format price product unit VND

--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -16,8 +16,15 @@ function Product({product}) {
             <Box padding={1}>
                 <img src={thumbnailUrl} alt={product.name} width="100%"/>
             </Box>
-            <Typography>{product.name}</Typography>
-            <Typography>{product.originalPrice}</Typography>
+            <Box padding={1}>
+                <Typography variant={"body2"}>{product.name}</Typography>
+                <Typography variant={"body2"}>
+                    <Box component={"span"} fontSize={"16px"} fontWeight={"bold"} mr={1}>
+                        {new Intl.NumberFormat('vi-VN', {style: 'currency', currency: 'VND'}).format(product.salePrice)}
+                    </Box>
+                    {(product.promotionPercent > 0) ? ` -${product.promotionPercent}%` : ''}
+                </Typography>
+            </Box>
         </Box>
 
     );
