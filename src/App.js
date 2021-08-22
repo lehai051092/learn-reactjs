b20b90ea85b95a6837fb2da930c64c2b6818c596
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-22 22:49:09 +0700

    Doing feature pagination

--- src/App.js
+++ src/App.js
@@ -4,28 +4,14 @@ import Counter from './components/Counter';
 import TodoFeature from './features/Todo';
 import {Redirect, Route, Switch} from 'react-router-dom';
 import NotFound from './components/NotFound';
-import {useEffect} from 'react';
-import productApi from './api/productApi';
 import CounterFeature from "./features/Counter";
 import Header from './components/Header';
 import ProductFeature from "./features/Product";
 
 function App() {
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const params = {
-                _limit: 10,
-            };
-            const productList = await productApi.getAll(params);
-            console.log(productList);
-        };
-
-        fetchProducts();
-    }, []);
-
     return (
         <div className="App">
-            <Header />
+            <Header/>
             <Switch>
                 <Redirect from="/home" to="/" exact/>
                 <Redirect from="/post-list/:postId" to="/posts/:postId" exact/>
--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -4,6 +4,7 @@ import {makeStyles} from "@material-ui/core/styles";
 import productApi from "../../../api/productApi";
 import ProductSkeletonList from "../components/ProductSkeletonList";
 import ProductList from "../components/ProductList";
+import {Pagination} from "@material-ui/lab";
 
 const useStyles = makeStyles(theme => ({
     root: {},
@@ -18,19 +19,37 @@ const useStyles = makeStyles(theme => ({
 function ListPage(props) {
     const classes = useStyles();
     const [productList, setProductList] = useState([]);
+    const [pagination, setPagination] = useState({
+        limit: 9,
+        total: 10,
+        page: 1
+    });
     const [loading, setLoading] = useState(true);
+    const [filters, setFilters] = useState({
+        _page: 1,
+        _limit: 9
+    });
 
     useEffect(() => {
         (async () => {
             try {
-                const {data} = await productApi.getAll({_page: 1, _limit: 10});
+                const {data, pagination} = await productApi.getAll(filters);
                 setProductList(data);
+                setPagination(pagination);
+                console.log(data, pagination);
             } catch (error) {
                 console.log(error);
             }
             setLoading(false);
         })();
-    }, []);
+    }, [filters]);
+
+    const handlePageChange = (e, page) => {
+        setFilters((prevFilters) => ({
+            ...prevFilters,
+            _page: page
+        }));
+    }
 
     return (
         <Box>
@@ -44,6 +63,12 @@ function ListPage(props) {
                     <Grid item className={classes.right}>
                         <Paper elevation={0}>
                             {loading ? <ProductSkeletonList/> : <ProductList productList={productList}/>}
+                            <Pagination
+                                color="primary"
+                                count={Math.ceil(pagination.total / pagination.limit)}
+                                page={pagination.page}
+                                onChange={handlePageChange}
+                            />
                         </Paper>
                     </Grid>
                 </Grid>
