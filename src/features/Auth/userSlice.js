be39bfec4aa41e2939acd85b77b7732899fdd178
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-22 09:00:16 +0700

    setup api login

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -14,6 +14,19 @@ export const register = createAsyncThunk(
     }
 )
 
+export const login = createAsyncThunk(
+    'user/login',
+    async (payload) => {
+        // call API to register
+        const data = await userApi.register(payload);
+        // save data to local storage
+        localStorage.setItem('access_token', data.jwt);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        // return user data
+        return data.user;
+    }
+)
+
 const userSlice = createSlice({
     name: 'user',
     initialState: {
@@ -23,6 +36,9 @@ const userSlice = createSlice({
     reducers: {},
     extraReducers: {
         [register.fulfilled]: (state, action) => {
+            state.current = action.payload;
+        },
+        [login.fulfilled]: (state, action) => {
             state.current = action.payload;
         }
     }
