e768191924bbe9801a04cb7e2ce61c30ed0d879b
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-24 12:50:28 +0700

    Style filter product price range

--- src/features/Product/components/Filters/FilterByPrice.jsx
+++ src/features/Product/components/Filters/FilterByPrice.jsx
@@ -1,12 +1,32 @@
 import React, {useState} from 'react';
 import {Box, Button, TextField, Typography} from "@material-ui/core";
 import PropTypes from "prop-types";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: theme.spacing(2)
+    },
+    range: {
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        alignItems: 'center',
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+
+        '& > span': {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+        }
+    }
+}));
 
 FilterByPrice.propTypes = {
     onChange: PropTypes.func,
 };
 
 function FilterByPrice({onChange}) {
+    const classes = useStyles();
     const [values, setValues] = useState({
         salePrice_gte: 0,
         salePrice_lte: 0
@@ -29,14 +49,14 @@ function FilterByPrice({onChange}) {
     }
 
     return (
-        <Box>
+        <Box className={classes.root}>
             <Typography variant={"subtitle2"}>Filter By Price</Typography>
-            <Box>
+            <Box className={classes.range}>
                 <TextField name="salePrice_gte" value={values.salePrice_gte} onChange={handleChange}/>
                 <span>-</span>
                 <TextField name="salePrice_lte" value={values.salePrice_lte} onChange={handleChange}/>
             </Box>
-            <Button variant={"outlined"} color={"primary"} onClick={handleSubmit}>Submit</Button>
+            <Button variant={"outlined"} size={"small"} color={"primary"} onClick={handleSubmit}>Submit</Button>
         </Box>
     );
 }
