7a1bbbebb539dbe7e71f6d6f829dd9e01badd86f
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-28 09:53:08 +0700

    Create actions for cart items

--- src/features/Cart/cartSlice.js
+++ src/features/Cart/cartSlice.js
@@ -10,10 +10,36 @@ const cartSlice = createSlice({
         showMiniCart(state) {
             state.showMiniCart = true;
         },
-
         hideMiniCart(state) {
             state.showMiniCart = false;
-        }
+        },
+        addToCart(state, action) {
+            // newItem = {id, product, quantity}
+            const newItem = action.payload;
+            const index = state.cartItems.findIndex((x) => x.id === newItem.id);
+
+            if (index >= 0) {
+                // increase quantity
+                state.cartItems[index].quantity += newItem.quantity;
+            } else {
+                // add to cart
+                state.cartItems.push(newItem);
+            }
+        },
+
+        setQuantity(state, action) {
+            const {id, quantity} = action.payload;
+            // Check if product available in cart
+            const index = state.cartItems.findIndex((x) => x.id === id);
+            if (index >= 0) {
+                state.cartItems[index].quantity = quantity;
+            }
+        },
+
+        removeFromCart(state, action) {
+            const idNeedToRemove = action.payload;
+            state.cartItems = state.cartItems.filter((x) => x.id !== idNeedToRemove);
+        },
     }
 });
 
